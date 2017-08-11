b94687c93143dc6e0fdd833c866cdc5634f72154
Kazuya Ueoka <info@fromkk.info>
2017-08-11 17:48:11 +0900

    Initial Commit

---
+++ LunchSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LunchSample
+//
+//  Created by Kazuya Ueoka on 2017/08/11.
+//  Copyright © 2017 fromKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
