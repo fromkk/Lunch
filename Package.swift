32990f4f2177e09d7602567c12f35df74175d662
Quang <quang.ha@rakuten.com>
2019-07-05 16:26:48 +0800

    add Lunch Test target

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "Lunch",
     products: [
-        .library(name: "Lunch", targets: ["Lunch"])
+        .library(name: "Lunch", targets: ["Lunch"]),
+        .library(name: "LunchTest", targets: ["LunchTest"])
     ],
     targets: [
         .target(
